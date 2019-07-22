58efe32c959b9fe7a6f36f29f07a72b44576e375
TheMCBrothers <50276703+TheMCBrothers@users.noreply.github.com>
2019-07-22 10:52:47 +0200

    Update download.lua

--- FileHandling/download.lua
+++ FileHandling/download.lua
@@ -1,5 +1,6 @@
 
 local protocol = "download_file"
+local w,h = term.getSize()
 
 local sModemSide = nil
 for n,sSide in pairs( rs.getSides() ) do
@@ -15,6 +16,10 @@ if sModemSide == nil then
 end
 
 rednet.open( sModemSide )
+
+local hosts = nil
+local tPaths = {}
+local pathIndex = 0
 
 function reset()
     term.setBackgroundColor(colors.black)
@@ -34,18 +39,50 @@ function error()
     print("Error: Download failed!")
 end
 
-function download(serverId,filePath,targetFilePath)
+function selectFromTable(tableIn,title)
+  if title == nil then title = "Select" end
+  local l = 1
+  local m = #tableIn
+  local host = tostring(tableIn[1])
+
+  while true do
+    term.setBackgroundColor(2048)
+    term.setTextColor(1)
+    term.clear()
+    term.setCursorPos(math.floor(w/2-#title/2),3)
+    term.write(title)
+
+    for n=1,m do
+      host = tostring(tableIn[n])
+      if l == n then host = "["..host.."]" end
+      term.setCursorPos(math.floor(w/2-#host/2), 5+n)
+      print(host)
+    end
+
+    local a,b = os.pullEvent("key")
+    if b == keys.up and l > 1 then l = l-1 end
+    if b == keys.down and l < m then l = l+1 end
+    if b == keys.enter then break end
+  end
+  return tableIn[l]
+end
+
+function download()
+    serverId = selectFromTable(hosts,"Select host id")
+    if serverId == nil then return end
     rednet.send(serverId,"request_download_file",protocol)
-    print("Requested server to download file.")
     id,msg,p = rednet.receive(protocol,5)
     if id == serverId then
         if msg == "ready_for_download" then
-            print("Server is ready for download.")
-            rednet.send(serverId,filePath,protocol)
+            id,paths,p = rednet.receive(p,5)
+            tPaths = textutils.unserialize(paths)
+            pathIndex = selectFromTable(tPaths)
+            rednet.send(serverId,pathIndex,protocol)
             id,content,p = rednet.receive(protocol,5)
             if id == serverId then
+                local targetFilePath = read()
                 if saveFile(targetFilePath,content) then
-				    print("Download success!")
+				            print("Download success!")
                     sleep(1)
                     reset()
                 end
@@ -58,23 +95,15 @@ function download(serverId,filePath,targetFilePath)
     end
 end
 
-function start()
-    reset()
-    term.setCursorPos(1,4)
-    term.write("Server-ID: ")
-    serverId = tonumber(read())
-    term.write("Server File Path: ")
-    filePath = read()
-    term.write("Target File Name: ")
-    targetFilePath = read()
-    term.setCursorPos(1,8)
-    term.write("confirm with 'yes'")
-    term.setCursorPos(1,7)
-    term.write("Are you sure? ")
-    if read() == "yes" then
-        term.setCursorPos(1,9)
-        download(serverId,filePath,targetFilePath)
-    end
+term.setBackgroundColor(2048)
+term.clear()
+term.setCursorPos(w/2-string.len("Searching for hosts...")/2,3)
+print("Searching for hosts...")
+hosts = { rednet.lookup(protocol) }
+if hosts == {} then
+  reset()
+  print("No hosts are available! :/")
+  return
+else
+  download()
 end
-
-start()
